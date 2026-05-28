c62a2007df4ac1854406972ee48bb6b303fd12c1
w8y56f <w8y56f@gmail.com>
2026-05-28 11:41:57 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,1 +1,26 @@
-1
+[
+    {
+        "Sid": "DenyUnencryptedObjectUploads",
+        "Effect": "Deny",
+        "Principal": "*",
+        "Action": "s3:PutObject",
+        "Resource": "arn:aws:s3:::你的存储桶名称/*",
+        "Condition": {
+            "StringNotEquals": {
+                "s3:x-amz-server-side-encryption": "aws:kms"
+            }
+        }
+    },
+    {
+        "Sid": "DenyPutsThatDoNotEncryptTheObject",
+        "Effect": "Deny",
+        "Principal": "*",
+        "Action": "s3:PutObject",
+        "Resource": "arn:aws:s3:::你的存储桶名称/*",
+        "Condition": {
+            "Null": {
+                "s3:x-amz-server-side-encryption": "true"
+            }
+        }
+    }
+]
