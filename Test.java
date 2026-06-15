014882a89f694eec9cf4237715881216b4abb554
w8y56f <w8y56f@gmail.com>
2026-06-15 17:08:42 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,13 +1,1 @@
-<changeSet id="add_idx_t_sms_info_policy_no" author="stone">
-    
-    <preConditions onFail="MARK_RAN">
-        <not>
-            <indexExists indexName="idx_t_sms_info_policy_no" tableName="T_SMS_INFO"/>
-        </not>
-    </preConditions>
 
-    <createIndex indexName="idx_t_sms_info_policy_no" tableName="T_SMS_INFO">
-        <column name="POLICY_NO"/>
-    </createIndex>
-    
-</changeSet>
