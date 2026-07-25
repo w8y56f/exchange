dd049b7bf2510f11e2c07564bd5a03a6de21662e
w8y56f <w8y56f@gmail.com>
2026-07-25 18:19:28 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,1 +1,19 @@
+kcd() {
+    if [ -z "$1" ]; then
+        current_ns=$(kubectl config view --minify --output 'jsonpath={..namespace}')
+        #echo "Current namespace1111: $current_ns"
+        display_ns=${current_ns:-default}
+        echo "Usage: kcd <namespace_name>"
+        echo "Current namespace: $display_ns"
+        return 1
+    fi
 
+    # Check if the provided namespace exists
+    if kubectl get namespace "$1" &> /dev/null; then
+        kubectl config set-context --current --namespace="$1" &> /dev/null
+        echo "Switched to namespace '$1'"
+    else
+        echo "Error: Namespace '$1' does not exist."
+        return 1
+    fi
+}
