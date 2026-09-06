4027cd66e1a8083b94d2b2f4436b99ca47ce4f8b
w8y56f <w8y56f@gmail.com>
2026-09-06 21:39:26 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,1 +1,56 @@
-Ili10oO
+ private Long listId;
+    private Long itemId;
+    private String extraType;
+    private Date startDate;
+    private Date endDate;
+    private Long policyId;
+    private String extraArith;
+    private BigDecimal extraPrem;
+    private BigDecimal extraPara;
+    private BigDecimal nextExtraPrem;
+    private Long emValue;
+    private String reason;
+    private Long insuredId;
+    private String extraStatus;
+    private String reCalcIndi;
+    private BigDecimal extraPremAn;
+    private BigDecimal nextExtraPremAn;
+    private Long emCi;
+    private Long emLife;
+    private Long emTpd;
+    private Long emsVersion;
+    private Long insertedBy;
+    private Long updatedBy;
+    private Date insertTime;
+    private Date updateTime;
+    private Date insertTimestamp;
+    private Date updateTimestamp;
+    private BigDecimal nextExtraPara;
+    private String dynamicFields;
+
+
+
+
+
+
+private Long exclusionId;
+    private String exclusionCode;
+    private Long policyId;
+    private String descLang1;
+    private String descLang2;
+    private Date effectDate;
+    private Date reviewDate;
+    private Date expiryDate;
+    private Long reviewPeriod;
+    private Long emsVersion;
+    private Long insertedBy;
+    private Long updatedBy;
+    private Date insertTime;
+    private Date updateTime;
+    private Date insertTimestamp;
+    private Date updateTimestamp;
+    private Long itemId;
+    private Long productId;
+    private Long insuredId;
+    private String dynamicFields;
+
